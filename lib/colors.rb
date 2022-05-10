c9e9f4558b968a5239ae597ad5970af7803c97de
1lubo <1lubo@pm.me>
2022-05-10 09:20:30 +0200

    add color for highlighting pieces

--- lib/colors.rb
+++ lib/colors.rb
@@ -1,4 +1,5 @@
 class String
     def black;          "\033[100m#{self}\033[0m" end
-    def blue;           "\033[104m#{self}\033[0m" end    
+    def blue;           "\033[104m#{self}\033[0m" end
+    def highlight;     "\033[106m#{self}\033[0m" end
 end

5fe1e9d2895ae840f76c20d3b98c39e0fee5611c
1lubo <1lubo@pm.me>
2022-05-11 09:26:58 +0200

    formatting fixes

--- lib/player.rb
+++ lib/player.rb
@@ -1,82 +1,78 @@
+# frozen_string_literal: true
+
 require_relative './board.rb'
 require_relative './modules.rb'
 
 class Player
+  include NotationTranslation
+  attr_accessor :color, :board
 
-    include NotationTranslation
-    attr_accessor :color, :board
+  def initialize(color, board)
+    @color = color
+    @board = board
+  end
 
-    def initialize(color, board)
-        @color = color
-        @board = board    
+  def get_input(info)
+    loop do
+      puts info
+      user_input = gets.chomp.downcase
+
+      return user_input if user_input.match?(/^[a-h]{1}[1-8]{1}$/)
+
+      puts 'Input error! Please enter a file (a - h) and rank (1 - 8).'
     end
+  end
 
-    def get_input(info)
-        loop do
-            puts info
-            user_input = gets.chomp.downcase   
+  def get_piece
+    info = 'Enter the coordinates of the piece you want to move.'
+    selection = nil
 
-            return user_input if user_input.match?(/^[a-h]{1}[1-8]{1}$/)                
-  
-            puts "Input error! Please enter a file (a - h) and rank (1 - 8)."            
-        end
-    end    
+    until selection
 
-    def get_piece  
-        info = "Enter the coordinates of the piece you want to move."
+      input = get_input(info)
+      coordinates = notation_to_coordinates(input)
+      selection = board.board[coordinates[0]][coordinates[1]]
+
+      if selection.nil? # selected square is empty
+        puts 'Selected sqaure is empty.'
+        next
+      else
+        selection.refresh_possible_moves
+      end
+
+      if selection.color != color # selected piece is opposite color
+        puts 'Selected piece is of opposite color.'
         selection = nil
+        next
+      end
 
-        until selection do
-
-            input = get_input(info)
-            coordinates = notation_to_coordinates(input)
-            selection = board.board[coordinates[0]][coordinates[1]]
-
-            if selection.nil? # selected square is empty
-                puts "Selected sqaure is empty."
-                next
-            else 
-                selection.refresh_possible_moves
-            end
-
-            if selection.color != color # selected piece is opposite color
-                puts "Selected piece is of opposite color."
-                selection = nil
-                next
-            end
-
-            if selection.possible_moves.empty?
-                puts "This piece does not have any valid moves."
-                selection = nil
-                next
-            else
-                selection.refresh_possible_moves
-            end
-        end
-
-        
-        return selection
+      if selection.possible_moves.empty?
+        puts 'This piece does not have any valid moves.'
+        selection = nil
+        next
+      else
+        selection.refresh_possible_moves
+      end
     end
 
-    def get_target(piece)
-        info = "Enter target coordinates"
-        target = nil       
+    selection
+  end
 
-        until target do
-            input = get_input(info)
-            target = notation_to_coordinates(input)
+  def get_target(piece)
+    info = 'Enter target coordinates'
+    target = nil
 
-            if !piece.possible_moves.include?(target)
-                puts "Invalid coordinates."
-                target = nil
-                next
-            else
-                return target
-            end
-        end
+    until target
+      input = get_input(info)
+      target = notation_to_coordinates(input)
 
+      if !piece.possible_moves.include?(target)
+        puts 'Invalid coordinates.'
+        target = nil
+        next
+      else
+        return target
+      end
     end
-
-
-
+  end
 end
